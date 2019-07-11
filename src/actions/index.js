05856017d8a98eb464d862b051433027a07468e5
fedka <fedkakatorjniy@gmail.com>
2019-07-11 13:11:13 +0300

    change loading

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,13 @@ const booksLoaded = (newBooks) => {
   };
 };
 
+const bookRequested = () => {
+	return {
+		type: 'BOOKS_REQUESTED',
+	};
+};
+
 export {
-  booksLoaded
+  booksLoaded,
+  bookRequested
 };
--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -5,13 +5,14 @@ import BookListItem from '../book-list-item';
 import './book-list.css';
 
 import { withBookstoreService } from '../hoc';
-import { booksLoaded } from '../../actions';
+import { booksLoaded, bookRequested } from '../../actions';
 import { compose } from '../../utils';
 import Spinner from '../spinner';
 
 class BookList extends Component {
 	componentDidMount() {
-		const { bookstoreService, booksLoaded } = this.props;
+		const { bookstoreService, booksLoaded, bookRequested } = this.props;
+		bookRequested();
 		bookstoreService.getBooks()
 			.then((data) => booksLoaded(data));
 	}
@@ -43,7 +44,8 @@ const mapStateToProps = ({ books, loading }) => {
 };
 
 const mapDispatchToProps = {
-	booksLoaded
+	booksLoaded,
+	bookRequested
 };
 
 export default compose(
